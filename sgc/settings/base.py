f8e1390fe91f10ef29e41a43954b930eaf6ce813
AgentPurpleLord <37461728+AgentPurpleLord@users.noreply.github.com>
2020-04-23 22:40:21 +1000

    Local Host Issue

--- sgc/settings/base.py
+++ sgc/settings/base.py
@@ -175,7 +175,7 @@ CORS_ORIGIN_WHITELIST = [
     "https://example.com",
     "https://sub.example.com",
     "http://localhost:8080",
-    "http://127.0.0.1:9000",
+    "http://127.0.0.1:8000",
     "http://localhost:8000",
     "http://localhost",
 ]
