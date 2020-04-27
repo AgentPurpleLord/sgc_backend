adea27719489344bc705f68999d352c981c3bb51
AgentPurpleLord <37461728+AgentPurpleLord@users.noreply.github.com>
2020-04-27 20:47:08 +1000

    More settings

--- sgc/settings/production.py
+++ sgc/settings/production.py
@@ -2,6 +2,12 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SECRET_KEY = '^ld6x(%wz3%0--$5&cc9jydadh7f#v6e*8%a@foq&*2z#@y&zc'
+
 WAGTAILAPI_BASE_URL = "https://startgamecrusaders.com/"
 
 try:
