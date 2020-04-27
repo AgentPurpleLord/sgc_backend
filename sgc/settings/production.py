64e08563e323fdaf584af36527bce86cec532df6
AgentPurpleLord <37461728+AgentPurpleLord@users.noreply.github.com>
2020-04-27 20:54:54 +1000

    Even more settings.

--- sgc/settings/production.py
+++ sgc/settings/production.py
@@ -9,6 +9,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SECRET_KEY = '^ld6x(%wz3%0--$5&cc9jydadh7f#v6e*8%a@foq&*2z#@y&zc'
 
 WAGTAILAPI_BASE_URL = "https://startgamecrusaders.com/"
+BASE_URL = 'https://startgamecrusaders.com'
+USE_X_FORWARDED_HOST = True
 
 try:
     from .local import *
